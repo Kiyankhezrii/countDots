792a16dbeeeab0fe17dac74bf6976cdeda1e2a8b
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-30 23:27:34 +0430

    get elements from DOM

---
+++ main.js
@@ -0,0 +1,6 @@
+// get element from dom
+const form=document.querySelector(".form")
+const input=document.querySelector(".form input");
+const btn=document.querySelector(".fa-magnifying-glass")
+const numberDots=document.querySelector(".count-dots span")
+
