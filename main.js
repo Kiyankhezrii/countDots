0ad1e0e918263f5a04c66f01f5d4803c509ac818
Kiyankhezri <kiyankhezrii96@gmail.com>
2022-07-30 23:58:59 +0430

    add alphabet and dots

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
 // get element from dom
-const form=document.querySelector(".form")
-const input=document.querySelector(".form input");
-const btn=document.querySelector(".fa-magnifying-glass")
-const numberDots=document.querySelector(".count-dots span")
+const form = document.querySelector(".form");
+const input = document.querySelector(".form input");
+const btn = document.querySelector(".fa-magnifying-glass");
+const numberDots = document.querySelector(".count-dots span");
 
+const alphabet = [
+  "ا",
+  "ب",
+  "پ",
+  "ت",
+  "ث",
+  "ج",
+  "چ",
+  "ح",
+  "خ",
+  "د",
+  "ذ",
+  "ر",
+  "ز",
+  "ژ",
+  "س",
+  "ش",
+  "ص",
+  "ض",
+  "ط",
+  "ظ",
+  "ع",
+  "غ",
+  "ف",
+  "ق",
+  "ک",
+  "گ",
+  "ل",
+  "م",
+  "ن",
+  "و",
+  "ه",
+  "ی",
+];
+
+const numberDotsAlphabet = [
+  0, 1, 3, 2, 3, 1, 3, 0, 1, 0, 1, 0, 1, 3, 0, 3, 0, 1, 0, 1, 0, 1, 1, 2, 0, 0,
+  0, 0, 1, 0, 0, 0,
+];
